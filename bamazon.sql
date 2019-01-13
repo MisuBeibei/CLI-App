e2ee0176afb3611799d7664e503502d28420961d
unknown <39747964+MisuBeibei@users.noreply.github.com>
2019-01-13 03:11:47 -0800

    edit README.md

--- bamazon.sql
+++ bamazon.sql
@@ -3,11 +3,11 @@ USE bamazon;
 
 CREATE TABLE products (
 	item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
-	PRIMARY KEY (item_id),
 	product_name VARCHAR(30) NOT NULL,
 	department_name VARCHAR(20) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
-	stock_quantity INTEGER(10) NOT NULL
+	stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
